737f8e5d4fd5fe672a327745254b60707dbb43b1
Xikly <107074986+Xikly@users.noreply.github.com>
2024-05-28 06:29:36 +0100

    Added automatic reconnecting functionality.

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from tqdm import tqdm
 from stage import jam
 from screeninfo import get_monitors
 
+import pyautogui
+import cv2
+import easyocr
+
+import json
+import os
+
+def getMainMonitor():
+    for m in get_monitors():
+        if m.is_primary is True:
+            return m
+
 class checks():
     def __init__(self) -> None:
         self.client_name = "FortniteClient-Win64-Shipping.exe"
@@ -17,9 +29,7 @@ class checks():
         -> bool (True or False if the service is running.)
         '''
         if self.client_name in (i.name() for i in psutil.process_iter()): # Returns bool if service_name is found in active services.
-            for m in get_monitors():
-                if m.is_primary is True:
-                    monitor = m
+            monitor = getMainMonitor()
             
             return monitor.height, monitor.width
         
@@ -27,8 +37,92 @@ class checks():
             print("Fortnite isn't running, open fortnite and restart the program.")
             exit()
 
+class disconnect():
+    def __init__(self) -> None:
+        self.monitor = getMainMonitor()
+
+        with open("discpos.json") as f:
+            self.dump = json.load(f)
+    
+    def beginLoop(self):
+        times_ran = 0
+        if self.reconnect() is True:
+            while True:
+                if self.CheckTrueReconnect() is True:
+                    print(f"Success! You've been reconnected...\nIt took {times_ran * 5} seconds to reconnect you.\n")
+                    return True
+
+                times_ran += 1
+
+                print(f"Checked {times_ran} time/s if you've been successfully reconnected.")
+                time.sleep(5)
+                
+        else:
+            print("There was an error reconnecting, exitting program.")
+            os._exit(0)
+            
+
+    def checkTrue(self) -> bool:
+        screenshot_conf = self.dump[str(self.monitor.height)]["screenshot"]
+
+        img1 = pyautogui.screenshot(region=(screenshot_conf[0], screenshot_conf[1], screenshot_conf[2], screenshot_conf[3]))
+        img1.save("dump/check_disconnect.png")
+        img = cv2.imread("dump/check_disconnect.png")
+
+        # Text detect instance
+        reader = easyocr.Reader(["en"], gpu = False, verbose = False)
+
+        # Detect text
+        detect = reader.readtext(img)
+
+        if not detect:
+            return False
+        else:
+            for t in detect:
+                bbox, text, score = t
+
+                if text == "PLAY":
+                    return True
+
+    def reconnect(self) -> bool:
+        try:
+            reconnect_conf = self.dump[str(self.monitor.height)]["reconnect"]
+
+            pyautogui.dragTo(reconnect_conf[0], reconnect_conf[1])
+            time.sleep(1)
+            pyautogui.click()
+        except Exception as error:
+            print(error)
+            return False
+
+        return True
+    
+    def CheckTrueReconnect(self) -> bool:
+        screenshot_conf = self.dump[str(self.monitor.height)]["check_reconnect"]
+
+        img1 = pyautogui.screenshot(region=(screenshot_conf[0], screenshot_conf[1], screenshot_conf[2], screenshot_conf[3]))
+        img1.save("dump/reconnect_check.png")
+        img = cv2.imread("dump/reconnect_check.png")
+
+        # Text detect instance
+        reader = easyocr.Reader(["en"], gpu = False, verbose = False)
+
+        # Detect text
+        detect = reader.readtext(img)  
+ 
+
+        if not detect:
+            return False
+        else:
+            for t in detect:
+                bbox, text, score = t
+
+                if text == "Play Loop":
+                    return True
+
 def main():
     height, width = checks().active()
+    discon = disconnect()
 
     time_ran = 0
 
@@ -40,7 +134,13 @@ def main():
         for x in tqdm(range(60)):
             time.sleep(1)
 
-        time_ran+=1
+        if discon.checkTrue() is True:
+            print("You've been disconnected, trying to reconnect now...")
+            discon.beginLoop()
+
+        time_ran += 1
+
+        print(f"Checked {time_ran} time/s if you've been disconnected...")
 
 if __name__ == "__main__":
     main()
